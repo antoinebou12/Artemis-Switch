2bdf9ea7115c7d4ac9dff01a209b1c0ca8b42099
Antoine Boucher <antoine.bou13@gmail.com>
2026-08-08 17:01:59 -0400

    build: compile advanced stream option store

--- app/src/artemis_advanced_stream_options.cpp
+++ app/src/artemis_advanced_stream_options.cpp
@@ -1,2 +1,3 @@
 // Main-target compilation bridge for Artemis advanced stream options.
 #include "features/stream/AdvancedStreamOptions.cpp"
+#include "features/stream/AdvancedStreamOptionsStore.cpp"
