a1b8364e28f012396b69514896f3849bfc4f9abc
Antoine Boucher <antoine.bou13@gmail.com>
2026-08-08 16:59:11 -0400

    apollo: add conservative metadata capability detection

--- app/src/host/HostCapabilities.hpp
+++ app/src/host/HostCapabilities.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 namespace artemis::host {
 
 enum class HostKind { Unknown, Sunshine, Apollo };
@@ -11,12 +13,21 @@ struct HostCapabilities {
     bool serverCommands = false;
     bool clipboardSync = false;
     bool inputOnly = false;
+    std::string detectionReason;
+};
+
+struct HostMetadata {
+    std::string appVersion;
+    std::string gfeVersion;
+    std::string gsVersion;
+    std::string advertisedExtensions;
 };
 
 class HostCapabilityPolicy {
 public:
     static HostCapabilities standardSunshine();
     static HostCapabilities apollo();
+    static HostCapabilities detect(const HostMetadata& metadata);
 };
 
 } // namespace artemis::host
