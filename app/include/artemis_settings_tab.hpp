fea881191dddadd0c233edf5cc7c9e30019c2edb
Antoine Boucher <antoine.bou13@gmail.com>
2026-08-08 17:04:33 -0400

    ui: expose optional Artemis feature controls

--- app/include/artemis_settings_tab.hpp
+++ app/include/artemis_settings_tab.hpp
@@ -14,10 +14,24 @@ private:
     void editWidth();
     void editHeight();
     void editBitrate();
+    void refreshFrameRateSelector();
 
     BRLS_BIND(brls::BooleanCell, customResolution, "custom_resolution");
     BRLS_BIND(brls::DetailCell, width, "custom_width");
     BRLS_BIND(brls::DetailCell, height, "custom_height");
     BRLS_BIND(brls::DetailCell, exactBitrate, "exact_bitrate");
     BRLS_BIND(brls::DetailCell, activeProfile, "active_profile");
+
+    BRLS_BIND(brls::SelectorCell, frameRate, "frame_rate");
+    BRLS_BIND(brls::BooleanCell, unlockHighFps, "unlock_high_fps");
+    BRLS_BIND(brls::BooleanCell, forceFullRange, "force_full_range");
+    BRLS_BIND(brls::BooleanCell, preventPacketLoss, "prevent_packet_loss");
+
+    BRLS_BIND(brls::SelectorCell, scaleMode, "scale_mode");
+
+    BRLS_BIND(brls::BooleanCell, forwardMotion, "forward_motion");
+    BRLS_BIND(brls::BooleanCell, consoleMotionFallback, "console_motion_fallback");
+
+    BRLS_BIND(brls::BooleanCell, rememberZoomPan, "remember_zoom_pan");
+    BRLS_BIND(brls::DetailCell, resetZoomPan, "reset_zoom_pan");
 };
