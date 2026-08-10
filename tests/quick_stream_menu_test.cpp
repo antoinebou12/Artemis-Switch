1c8194a751a6db2afe037d921c1bce8706c7bb7e
Antoine Boucher <antoine.bou13@gmail.com>
2026-08-10 13:02:11 -0400

    test: cover minimal quick actions

--- tests/quick_stream_menu_test.cpp
+++ tests/quick_stream_menu_test.cpp
@@ -4,17 +4,18 @@
 #include <cassert>
 
 using artemis::ui::QuickAction;
-using artemis::ui::QuickMenuContext;
 using artemis::ui::buildQuickActions;
 
 int main() {
-    auto actions = buildQuickActions({false, true});
-    assert(std::find(actions.begin(), actions.end(), QuickAction::StartBenchmark) != actions.end());
-    assert(std::find(actions.begin(), actions.end(), QuickAction::StopBenchmark) == actions.end());
+    auto actions = buildQuickActions({true});
+    assert(std::find(actions.begin(), actions.end(), QuickAction::ToggleKeyboard) != actions.end());
+    assert(std::find(actions.begin(), actions.end(), QuickAction::TogglePerformanceStats) != actions.end());
+    assert(std::find(actions.begin(), actions.end(), QuickAction::ToggleMouseMode) != actions.end());
+    assert(std::find(actions.begin(), actions.end(), QuickAction::Disconnect) != actions.end());
     assert(actions.back() == QuickAction::QuitHostApp);
 
-    actions = buildQuickActions({true, false});
-    assert(std::find(actions.begin(), actions.end(), QuickAction::StopBenchmark) != actions.end());
+    actions = buildQuickActions({false});
     assert(std::find(actions.begin(), actions.end(), QuickAction::QuitHostApp) == actions.end());
+    assert(actions.back() == QuickAction::Disconnect);
     return 0;
 }
